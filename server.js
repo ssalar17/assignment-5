4d31c1b7e119e5e16aac695f6a479d600bf7cc1f
ssalar17 <ssalar@myseneca.ca>
2020-08-06 22:39:28 -0400

    test 2

--- server.js
+++ server.js
@@ -123,6 +123,9 @@ app.post("/signup", (req,res)=>{
             res.redirect("/");
         }
     })
-    app.listen(3000, ()=>{
+
+    const PORT = process.env.PORT || 3000;
+
+    app.listen(PORT, ()=>{
         console.log("Web Server is up and running");
     });
